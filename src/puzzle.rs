62eec945edd77428462eb39875062737901235f5
Mattias <1116150+Mattias1@users.noreply.github.com>
2022-05-01 18:13:24 +0200

    Some improvements thanks to suggestions
    - More spacing between the nummbers
    - A congratulations message when the puzzle is solved
    - Be able to mark off numbers

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -7,7 +7,7 @@ pub struct Puzzle {
 
 impl Puzzle {
   pub fn build(name: &str, puzzle: &Vec<Vec<i32>>) -> Puzzle {
-    let height = if let Some(v) = puzzle.get(0) { v.len() } else { 0};
+    let height = if let Some(v) = puzzle.get(0) { v.len() } else { 0 };
     Puzzle {
       name: String::from(name),
       answers: puzzle.to_vec(),
@@ -49,45 +49,56 @@ impl Puzzle {
     self.values[y][x] = if self.values[y][x] == 2 { 0 } else { 2 };
   }
 
-  pub fn row_nrs(&self, y: usize) -> String {
-    let mut result = String::new();
+  pub fn row_nrs(&self, y: usize) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::new();
     let mut curr = 0;
     for x in 0..self.width() {
       if self.answers[y][x] == 1 {
         curr += 1;
       } else if curr > 0 {
-        result += &curr.to_string();
-        result.push(' ');
+        result.push(curr);
         curr = 0;
       }
     }
     if curr > 0 {
-      result += &curr.to_string();
+      result.push(curr);
     }
     result
   }
 
-  pub fn col_nrs(&mut self, x: usize) -> String {
-    let mut result = String::new();
+  pub fn col_nrs(&mut self, x: usize) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::new();
     let mut count = 0;
     let mut curr = 0;
     for y in 0..self.height() {
       if self.answers[y][x] == 1 {
         curr += 1;
       } else if curr > 0 {
-        result += &curr.to_string();
+        result.push(curr);
         count += 1;
-        result.push('\n');
         curr = 0;
       }
     }
     if curr > 0 {
-      result += &curr.to_string();
+      result.push(curr);
       count += 1;
     }
     if count > self.max_col_count {
       self.max_col_count = count;
     }
     result
+  }
+
+  pub fn is_solved(&self) -> bool {
+    for y in 0..self.height() {
+      for x in 0..self.width() {
+        if self.answers[y][x] == 0 && self.values[y][x] == 1 {
+          return false
+        } else if self.answers[y][x] == 1 && self.values[y][x] != 1 {
+          return false
+        }
+      }
+    }
+    true
   }
 }
